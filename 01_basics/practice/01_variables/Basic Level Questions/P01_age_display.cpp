52150cae561d088d7e7b7664a7cec5293a57af43
Abhinav <abhinavchaturvedi.swe@gmail.com>
2025-11-14 23:32:22 +0530

    Practice some variable questions

---
+++ 01_basics/practice/01_variables/Basic Level Questions/P01_age_display.cpp
@@ -0,0 +1,13 @@
+//Declare a variable to store your age and print it.
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int age;//Declaration
+    age = 18;//Initialization
+
+    cout <<"age = "<< age <<endl;
+
+    return 0;
+}
---
+++ 01_basics/practice/01_variables/Basic Level Questions/P02_mixed_variables_print.cpp
@@ -0,0 +1,20 @@
+//Declare three variables of different types (int, float, char) and display them.
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int age;
+    float height;
+    char grade;
+
+    age = 18;
+    height = 5.6;
+    grade = 'A';
+
+    cout << "age = " << age <<endl;
+    cout << "height = " << height <<endl;
+    cout << "grade = " << grade <<endl;
+
+    return 0;
+}
